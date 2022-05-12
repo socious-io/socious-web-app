80e175d1f3ca6e62c0a7103292a1459282231174
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2022-05-13 00:26:10 +0800

    chore: connect to metamask

--- frontend-next/pages/_app.tsx
+++ frontend-next/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../asset/css/global.css";
 import "../styles/index.css";
 import "../styles/App.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
+function getLibrary(provider: any) {
+  return new Web3(provider);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -22,7 +28,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
-      <Component {...pageProps} />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
     </>
   );
 }
