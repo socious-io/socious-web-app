8ce875102ede8312f1b37a671059076e9221dbe4
Babin Bohora <bohorababin1@gmail.com>
2022-09-05 09:36:28 +0900

    useUser with forgot-password logic

--- frontend-next/services/useUser.ts
+++ frontend-next/services/useUser.ts
@@ -4,60 +4,70 @@ import {ApiConstants } from 'utils/api';
 import useSWR from 'swr'
 import { useEffect } from 'react';
 import Router, { useRouter } from 'next/router';
-import path from 'path';
 
 interface UseUserProps {
-  redirectIfFound?: boolean;
-  redirect?: boolean;
-  redirectTo?: string;
   shouldRetry?: boolean;
   onAuthError?: boolean;
   forceStop?: boolean
-}
+};
 
-const useUser = ({
-    redirectIfFound = false,
-    redirectTo = "",
-    // Force Stop redirect logic.
-    // Implemented to try Password expired
-    forceStop = false,
+const defaultValues = {
+  // Force Stop redirect logic.
+  // Implemented to try Password expired
+  forceStop: false,
+  shouldRetry: true,
+  onAuthError: true
+};
 
-    shouldRetry = true,
-    onAuthError = true
-  }: UseUserProps
+const allowedRoutes = [
+  "/", "/auth/forgotpassword", "/auth/login", "/auth/signup"
+];
+
+const useUser = (props: UseUserProps = 
+  defaultValues
 ) => {
+  const { forceStop, shouldRetry, onAuthError } = {...defaultValues, ...props};
+  const { pathname } = useRouter();
+
   const { data: user, error: userError, mutate: mutateUser } = useSWR<any>("/api/v2/user/profile", get, {
     shouldRetryOnError: shouldRetry,
     onErrorRetry: (error) => {
       if (!onAuthError && error.response.status === 401) return
     }
   });
-  const { pathname } = useRouter();
+
 
   useEffect(() => {
     // if user && error both are undefined
     if (!user && !userError) return
-    
     if (forceStop) return
 
-    // if user unauthorized || or any error
+    // if user unauthorized
     if (userError && userError.response.status === 401) {
-      // if page !== home
-      if (pathname !== "/") {
-        // => redirect to /auth/login 
-        Router.push("/auth/login");
+      // if page !== allowed_routes
+      if (!(allowedRoutes.includes (pathname))) {
         // => add ?redirect_to=/profile
+        Router.push("/auth/login");
       }
     }
 
     
     // if user authorized
-      // if page === home [doesn't need to be home]
+    if (user) {
+      // if user has requested forgot password.
+      if (user.password_expired) {
+        Router.push("/auth/forgotpassword");
+        return
+      } else if (pathname.startsWith("/auth/forgotpassword")) {
+        // if user trying to access /auth/forgotpassword when password not expired
+        Router.push("/");
+      }
+
       // if user === new_user
-    if (user && !user.skills && !user.passions) {
-      //=> redirect to /auth/onboarding
-      Router.push("/auth/onboarding");
-    }    
+      if (!user.skills && !user.passions) {
+        Router.push("/auth/onboarding");
+      }    
+    }
 
   }, [user, pathname, userError, forceStop])
   return {user, userError, mutateUser};
