c38c2009803ef0adb8cdd75cfe1b30f25551b44f
Babin Bohora <bohorababin1@gmail.com>
2022-09-12 11:26:56 +0900

    Post/edit done

--- frontend-next/components/common/Post/EditModal/EditModal.tsx
+++ frontend-next/components/common/Post/EditModal/EditModal.tsx
@@ -10,31 +10,33 @@ import {
 import { XIcon } from "@heroicons/react/outline";
 import { joiResolver } from "@hookform/resolvers/joi";
 import { useUser } from "@hooks";
-import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
 import { get } from "utils/request";
 import CausesTagBar from "../CausesTagBar/CausesTagBar";
 import PostActionBar from "../PostActionBar/PostActionBar";
 import { EditPostBodyType } from "@models/post";
+import { uploadMedia } from "@api/media/actions";
+import { editPost } from "@api/posts/actions";
+import { PostCard } from "layout/screen/PostCard";
+
+interface EditModalProps extends ModalProps {
+  pid: string;
+}
 
 const EditModal = ({
   isOpen = false,
   onClose = () => null,
-  children,
-  className,
-}: ModalProps) => {
+  pid,
+}: EditModalProps) => {
   const { user } = useUser();
-  const router = useRouter();
-  const { pid } = router.query;
-  const { data: post, error: postError, mutate } = useSWR<any>(`/api/v2/posts/${pid}`, get, {
+  const { data: post, error: postError, mutate } = useSWR<any>(pid ? `/api/v2/posts/${pid}` : null, get, {
     onErrorRetry: (error) => {
       if (error?.response?.status === 500 && error?.response?.data?.error.startsWith("invalid input syntax for type uuid")) return
     }
   });
 
   const [file, setFile] = useState<any>(null);
-  console.log("post ---->", post);
 
   const { handleSubmit, register, getValues, setValue, formState } = useForm({
     resolver: joiResolver(schemaEditPost),
@@ -45,17 +47,45 @@ const EditModal = ({
     },
   })
 
-  const onSubmit = useCallback(() => {
+  const onSubmit = useCallback( async() => {
     const content = getValues('content');
     const causes_tags = getValues('causes_tags');
     const link = getValues('link');
-    console.log({ content, causes_tags, link});
 
-    const postEditBody: EditPostBodyType = { content, causes_tags: [causes_tags]};    
-  }, [getValues]);
+
+    let fileId = "";
+    if (file) {
+      try {
+        // Image upload feature is not working.
+        const formData = new FormData;
+        formData.append("file", file);
+        const response: any = await uploadMedia(formData);
+        fileId = response?.id;
+      } catch(error) {
+        console.error(error);
+        return;
+      }
+    }
+
+    const postEditBody: EditPostBodyType = { content, causes_tags: [causes_tags]};
+    try {
+      await editPost(postEditBody, post.id);
+      onClose();
+      mutate();
+    } catch(error) {
+      console.error(error);
+    }  
+  }, [
+      getValues,
+      file,
+      post,
+      onClose,
+      mutate
+     ]
+  );
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} 
+    <Modal isOpen={isOpen} onClose={onClose}
       // className={`${shareStep === 1 ? "bg-offWhite" : ""}`}
     >
       <span className='absolute right-3 cursor-pointer ' onClick={onClose}>
@@ -69,23 +99,33 @@ const EditModal = ({
         <div className="mt-2">
           <CausesTagBar
             src={user?.avatar?.url}
-            preSelected={post && post.causes_tags[0]}
+            preSelected={post && post.causes_tags?.[0]}
             register={register('causes_tags')}
             errorMessage={formState?.errors?.['causes_tags']?.message}
           />
-          <div className="flex items-center -mr-6 -ml-6 p-4 space-x-4" >
-            <Avatar src={user?.avatar?.url} size="m" />
-            <span>{user?.first_name + " " + user?.last_name}</span>
-          </div>
           <TextArea
-            // placeholder='I feel like......'
-            rows={4}
-            containerClassName=" -mr-5 -ml-5"
+            rows={post?.shared_id ? 6 : 10}
+            containerClassName=" -mr-5 -ml-5 mt-4"
             className='focus:border-none'
             errorMessage={formState?.errors?.['content']?.message}
             register={register("content")}
           />
         </div>
+        {
+          !!(post?.shared_id) &&
+            <PostCard
+              id={post.shared_from_identity.meta.id}
+              name={post.shared_from_identity.meta.name}
+              content={post.shared_post.content}
+              time={post.shared_post.created_at}
+              passion={post.shared_post.causes_tags}
+              src={post.shared_from_identity.meta.avatar}
+              liked={post.shared_post.liked}
+              likes={post.shared_post.likes}
+              showAction={false}
+              shared={post.shared_post.shared}
+            />
+        }
       </Modal.Description>
       <PostActionBar
         register={register('link')}
@@ -98,7 +138,7 @@ const EditModal = ({
         variant="fill"
         value="Submit"
         >
-        Share Post
+        Edit Post
       </Button>
     </form>
     </Modal>
