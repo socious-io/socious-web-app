69aaf3b483a6192ff9156b55a3e3ba7272186a58
Lalo Martins <lalo.martins@gmail.com>
2022-09-13 18:43:48 +0300

    fix errors for login

--- frontend-next/utils/request.ts
+++ frontend-next/utils/request.ts
@@ -1,11 +1,10 @@
-import axios, {AxiosResponse} from 'axios';
-import {ErrorParamType, handleErrorAxios} from './helpers';
+import axios from 'axios';
 
-type AxiosRequestHeaders = {
+export type AxiosRequestHeaders = {
   [x: string]: string | number | boolean;
 };
 
-const request = axios.create({
+export const request = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE,
   withCredentials: true,
   timeout: 40000,
@@ -14,62 +13,58 @@ const request = axios.create({
   },
 });
 
-request.interceptors.response.use(
-  undefined,
-  (error) => {
-    throw new FetchError(handleErrorAxios(error));
-  },
-);
-
-async function get<T>(url: string) {
+export async function get<T>(url: string) {
   console.log(`get ${url}`);
   const response = await request.get<T>(url);
   return response.data;
 }
 
-async function deleteRequest<T>(url: string, headers?: AxiosRequestHeaders) {
+export async function deleteRequest<T>(
+  url: string,
+  headers?: AxiosRequestHeaders,
+) {
   const response = await request.delete<T>(url, headers);
   return response.data;
 }
 
-async function post<T>(url: string, data: any, headers?: AxiosRequestHeaders) {
+export async function post<T>(
+  url: string,
+  data: any,
+  headers?: AxiosRequestHeaders,
+) {
   const response = await request.post<T>(url, data, headers);
   return response.data;
 }
 
-async function patch<T>(url: string, data: any, headers?: AxiosRequestHeaders) {
+export async function patch<T>(
+  url: string,
+  data: any,
+  headers?: AxiosRequestHeaders,
+) {
   const response = await request.patch<T>(url, data, headers);
   return response.data;
 }
 
-async function put<T>(url: string, data: any, headers?: AxiosRequestHeaders) {
+export async function put<T>(
+  url: string,
+  data: any,
+  headers?: AxiosRequestHeaders,
+) {
   const response = await request.put<T>(url, data, headers);
   return response.data;
 }
 
-const all = axios.all;
-const spread = axios.spread;
+export const all = axios.all;
+export const spread = axios.spread;
 
-class FetchError extends Error {
-  response?: AxiosResponse<unknown>;
-  data: {
-    error: string;
-  };
-  code: string;
-  constructor({code, msg, response, data}: ErrorParamType<any>) {
-    // Pass remaining arguments (including vendor specific ones) to parent constructor
-    super(msg);
+export type ErrorMessage = {
+  title: string;
+  message: string;
+};
 
-    // Maintains proper stack trace for where our error was thrown (only available on V8)
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, FetchError);
-    }
-
-    this.name = 'FetchError';
-    this.response = response;
-    this.data = data || {error: msg};
-    this.code = code.toString();
-  }
-}
-
-export {get, post, put, deleteRequest, patch, all, spread, FetchError};
+export const DefaultErrorMessage: ErrorMessage = {
+  title: 'Sorry, something went wrong',
+  message:
+    'An unexpected error occurred. It has been reported to our team, ' +
+    "and we'll fix it as soon as we can. Please try again in a few minutes.",
+};
