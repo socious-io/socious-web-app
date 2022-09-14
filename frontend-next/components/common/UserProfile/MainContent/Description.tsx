d8b6256196e6335380630f9fe5fadbee02156ffc
hadis soleymani <hadissoleymani.it@gmail.com>
2022-09-14 10:35:14 -0700

    add with space to paragraphs

--- frontend-next/components/common/UserProfile/MainContent/Description.tsx
+++ frontend-next/components/common/UserProfile/MainContent/Description.tsx
@@ -16,7 +16,7 @@ const Description: React.FC<Props> = ({title, paragraph}) => {
   return (
     <div className="px-4 border-t border-grayLineBased ">
       <Title>{title}</Title>
-      <p className="text-graySubtitle w-4/6 mb-4 text-base">
+      <p className="text-graySubtitle w-4/6 mb-4 text-base whitespace-pre-line">
         {paragraph ? paragraph : 'No information'}
       </p>
     </div>
