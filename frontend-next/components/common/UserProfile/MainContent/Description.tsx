0360b1409481e03466d8756a6bc9278f39c0b1b3
hadis soleymani <hadissoleymani.it@gmail.com>
2022-09-14 10:42:05 -0700

    add break all for long links

--- frontend-next/components/common/UserProfile/MainContent/Description.tsx
+++ frontend-next/components/common/UserProfile/MainContent/Description.tsx
@@ -16,7 +16,7 @@ const Description: React.FC<Props> = ({title, paragraph}) => {
   return (
     <div className="px-4 border-t border-grayLineBased ">
       <Title>{title}</Title>
-      <p className="text-graySubtitle w-4/6 mb-4 text-base whitespace-pre-line">
+      <p className="text-graySubtitle w-4/6 mb-4 text-base whitespace-pre-line break-all ">
         {paragraph ? paragraph : 'No information'}
       </p>
     </div>
