4cf067912f78300bdb3260e2e94fb368243a2424
hadis soleymani <hadissoleymani.it@gmail.com>
2022-09-16 08:45:17 -0700

    change carousel size in mobile

--- frontend-next/components/common/CreateOrganization/components/Carousel.tsx
+++ frontend-next/components/common/CreateOrganization/components/Carousel.tsx
@@ -1,23 +1,21 @@
 import React, {useState} from 'react';
 //icons
 import {ChevronLeftIcon} from '@heroicons/react/outline';
-interface Props{
-  step:number;
-  onPress:()=>void;
+interface Props {
+  step: number;
+  onPress: () => void;
 }
-const Carousel:React.FC<Props> = ({step, onPress}) => {
- 
- 
+const Carousel: React.FC<Props> = ({step, onPress}) => {
   return (
-    <div className=" flex h-16 justify-center items-center border-b relative border-grayLineBased py-4">
+    <div className=" flex sm:h-16 h-20 justify-center items-center border-b relative border-grayLineBased py-4">
       <ChevronLeftIcon
         onClick={onPress}
-        className="h-6 w-6 text-black stroke-1.5 absolute left-4 hover:text-primary"
+        className="sm:h-6 sm:w-6 h-8 w-8 text-black stroke-1.5 absolute left-4 hover:text-primary"
       />
 
       {[1, 2, 3, 4, 5, 6].map((stepNumber) => (
         <span
-          className={`w-3 h-3 mx-1 cursor-pointer rounded-3xl transition-all duration-300 border border-grayLineBased ${
+          className={`sm:h-3 sm:w-3 h-4 w-4 mx-1 cursor-pointer rounded-3xl transition-all duration-300 border border-grayLineBased ${
             step === stepNumber && 'bg-primary'
           }`}
         />
