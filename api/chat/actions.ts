c067bdd3ea521895adf855e4251873abfdcb16ca
Babin Bohora <bohorababin1@gmail.com>
2022-09-19 23:13:37 +0900

    build fix

--- api/chat/actions.ts
+++ api/chat/actions.ts
@@ -1,4 +1,4 @@
-import {messageBodyType, createMessageResponseType} from '@models/chat';
+import {messageBodyType, createMessageResponseType} from '@models/message';
 import {post} from 'utils/request';
 
 export function sendMessage(chatId: string, messageBody: messageBodyType) {
