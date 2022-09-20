667496f368da3eaf0c05474c940dc53eceb528b9
hadis soleymani <hadissoleymani.it@gmail.com>
2022-09-20 02:21:31 -0700

    add layout changes

--- components/common/CreateOrganization/steps/OrganizationType.tsx
+++ components/common/CreateOrganization/steps/OrganizationType.tsx
@@ -6,35 +6,35 @@ import Button from '@components/common/Button/Button';
 import {StepProps} from '@models/stepProps';
 const items = Object.keys(Data.OrganizationType);
 const OrganizationType = ({onSubmit}: StepProps) => {
-
   const handleOnSubmit = (e: any) => {
-    
     e.preventDefault();
     onSubmit('true');
   };
   return (
     <>
       <Title>What type of organization?</Title>
-      <form className="h-full overflow-y-scroll">
-        <div className="px-4 py-2">
+      <form onSubmit={handleOnSubmit} className="flex h-full flex-col">
+        <div className="h-14 w-full grow overflow-y-scroll px-4  py-2">
           {items.map((item) => {
             return (
-              <button className="flex w-full items-center justify-between px-4 transition-all duration-300 focus:bg-secondary focus:pl-5 focus:text-white focus:rounded-2xl">
-                <p className="lowercase py-2 w-fit text-left text-lg sm:text-base">{item}</p>
-                <CheckCircleIcon className="h-6 w-6 ml-2 text-white stroke-1.5 " />
+              <button className="flex w-full items-center justify-between px-4 transition-all duration-300 focus:rounded-2xl focus:bg-secondary focus:pl-5 focus:text-white">
+                <p className="w-fit py-2 text-left text-lg lowercase sm:text-base">
+                  {item}
+                </p>
+                <CheckCircleIcon className="ml-2 h-6 w-6 stroke-1.5 text-white " />
               </button>
             );
           })}
         </div>
-      </form>
-      <footer className="flex pt-6 pb-28 sm:pb-10 sm:pt-4 justify-center border-grayLineBased border-t">
+        <footer className="w-full flex-none border-t border-grayLineBased pt-6 pb-28  sm:pb-10 sm:pt-4">
           <Button
             type="submit"
-            className="w-8/12 flex justify-center py-1.5 font-medium"
+            className="mx-auto flex w-8/12 justify-center py-1.5 font-medium"
           >
             continue
           </Button>
         </footer>
+      </form>
     </>
   );
 };
