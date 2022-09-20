2408b0d1883fda65a3daab64c64b957391dd8e68
“sooodi” <sudytaher@gmail.com>
2022-09-20 21:40:51 +0430

    project detail

--- pages/app/projects/[id].tsx
+++ pages/app/projects/[id].tsx
@@ -1,10 +1,10 @@
 import SideBar from '@components/common/Home/SideBar';
-import DetailContent from '@components/common/Project/HiredProjects/DetailContent';
+import DetailContent from '@components/common/Project/MainContent/DetailContent';
 
 const Detail = () => {
   return (
     <div className=" mx-6 mt-10 flex md:space-x-6">
-      {/* <SideBar selectBar={'PROJECT_DETAIL'} /> */}
+      <SideBar selectBar={'PROJECT_DETAIL'} />
       <DetailContent />
     </div>
   );
