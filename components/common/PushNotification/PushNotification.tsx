0b9cc732f50b81389ad4300c594c911225c8d297
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2022-09-21 18:01:10 +0800

    chore: watch payload

--- components/common/PushNotification/PushNotification.tsx
+++ components/common/PushNotification/PushNotification.tsx
@@ -32,15 +32,18 @@ function PushNotificationLayout({children}: any) {
   function getMessage() {
     const messaging = getMessaging();
     onMessage(messaging, (payload: MessagePayload) => {
-      toast(
-        <div>
-          <h5>{payload.notification?.title}</h5>
-          <h6>{payload.notification?.body}</h6>
-        </div>,
-        {
-          closeOnClick: false,
-        },
-      );
+      if (payload) {
+        console.log('payload===>', payload);
+        toast(
+          <div>
+            <h5>{payload.notification?.title}</h5>
+            <h6>{payload.notification?.body}</h6>
+          </div>,
+          {
+            closeOnClick: false,
+          },
+        );
+      }
     });
   }
 
