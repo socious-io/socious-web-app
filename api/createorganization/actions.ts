73f15ef31b7b90a1629060e3a7a0a84fdd3a6edc
hadis soleymani <hadissoleymani.it@gmail.com>
2022-09-23 14:02:26 -0700

    add create organization api

--- api/createorganization/actions.ts
+++ api/createorganization/actions.ts
@@ -1,6 +1,14 @@
 import {post} from 'utils/request';
 
-
-export function create_organization(data:any) {
-  return post('/auth/web/login', {data});
+export function create_organization(data: any) {
+  return post('/orgs', {
+    name: data.name,
+    bio: data.bio,
+    email: data.email,
+    phone: data.phone,
+    type: data.type,
+    city: data.city,
+    address: data.address,
+    social_causes: data.social_causes,
+  });
 }
