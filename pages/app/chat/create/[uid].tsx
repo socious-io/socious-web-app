617f7b099fab3185483253d4976bc185f7e507eb
Babin Bohora <bohorababin1@gmail.com>
2022-09-26 10:13:52 +0900

    Create new chat if not existing

--- pages/app/chat/create/[uid].tsx
+++ pages/app/chat/create/[uid].tsx
@@ -1,40 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
-import {FindChatBody} from '@models/chat';
-import {findChat} from '@api/chat/actions';
+import {CreateChatBodyType, FindChatBodyType} from '@models/chat';
+import {createChat, findChat} from '@api/chat/actions';
 import {GridLoader} from 'react-spinners';
 
 const ChatCreate = () => {
   const router = useRouter();
-  const {uid} = router.query;
+  const {uid, name} = router.query;
 
-  console.table({uid, name});
+  const [status, setStatus] = useState<string>('CHECKING FOR YOUR CHAT...');
 
   useEffect(() => {
-    if (!uid) return;
-    // router.push('/app/chat');
-    const toFindChat: FindChatBody = {
-      participants: [uid as string],
-    };
-    findChat(toFindChat)
-      .then((response) => {
-        console.log('RESPONSE of CHECK', response);
-        // IF chat exists,
-        if (response.items.length !== 0) {
-          // Redirect_to chat
-          router.push(`/app/chat/${response.items?.[0].id}`);
-        }
-        // If no chat
-        // Create new chat
-      })
-      .catch((error) => console.log('ERROR', error));
-  }, [router, uid]);
+    if (!uid) router.push('/app/chat');
 
-  // 6b108a14-a496-4f4d-b1c1-70dcd262f39c CMHC org
-  // a505d9b6-97cb-4784-a586-3067d73dbdcc bohorababin1@gmail.com
+    (async () => {
+      const toFindChat: FindChatBodyType = {
+        participants: [uid as string],
+      };
+
+      try {
+        const foundChats = await findChat(toFindChat);
+
+        // IF CHAT EXISTS
+        if (foundChats.items.length !== 0) {
+          // REDIRECT TO EXISTING CHAT
+          router.push(`/app/chat/${foundChats.items?.[0].id}`);
+        } else {
+          // IF NO CHAT EXISTS
+          setStatus('I see. Have a great conversation.');
+          const chatBody: CreateChatBodyType = {
+            participants: [uid as string],
+            type: 'CHAT',
+            name: (name as string) || 'nameless',
+          };
+
+          // CREATE NEW CHAT
+          const chatResponse: any = await createChat(chatBody);
+          router.push(`/app/chat/${chatResponse.id}`);
+        }
+      } catch (error) {
+        router.push('/app/chat');
+      }
+    })();
+  }, [name, router, uid]);
+
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center">
       <GridLoader color="#36d7b7" />
+      <p>{status}</p>
     </div>
   );
 };
