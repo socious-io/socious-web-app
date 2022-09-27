88cdf0b34cbf036453d7e9a1d92a2ddfe3cffd0f
Babin Bohora <bohorababin1@gmail.com>
2022-09-27 17:29:04 +0900

    Strong Password check

--- components/common/Auth/Signup/Step3/SignupStep3.tsx
+++ components/common/Auth/Signup/Step3/SignupStep3.tsx
@@ -1,6 +1,6 @@
 import {InputFiled, Button} from '@components/common';
 import {useState, useMemo, useCallback} from 'react';
-import {rxHasNumber} from 'utils/regex';
+import {rxHasLowerCase, rxHasNumber, rxHasUpperCase} from 'utils/regex';
 import {EyeIcon, EyeSlashIcon} from '@heroicons/react/24/outline';
 import {twMerge} from 'tailwind-merge';
 import {StepProps} from '@models/stepProps';
@@ -18,7 +18,7 @@ const SignupStep3 = ({onSubmit}: StepProps) => {
   const password = watch('password');
 
   const isValidPasswordLength = useMemo<boolean>(
-    () => password && password.length >= 7,
+    () => password && password.length >= 8,
     [password],
   );
 
@@ -26,13 +26,21 @@ const SignupStep3 = ({onSubmit}: StepProps) => {
     () => password && rxHasNumber.test(password),
     [password],
   );
+  const isValidPasswordHasUpCase = useMemo<boolean>(
+    () => password && rxHasUpperCase.test(password),
+    [password],
+  );
+  const isValidPasswordHasLowCase = useMemo<boolean>(
+    () => password && rxHasLowerCase.test(password),
+    [password],
+  );
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex grow flex-col justify-between pl-0 pr-10 sm:grow-0 sm:pl-10  "
+      className="flex grow flex-col justify-between pl-0 pr-10 sm:pl-10  "
     >
-      <div className="flex h-[28rem] flex-col">
+      <div className="sm:min-h[28rem] flex grow flex-col">
         <h1 className="font-helmet">Set your password</h1>
         <InputFiled
           label="Password"
@@ -41,7 +49,8 @@ const SignupStep3 = ({onSubmit}: StepProps) => {
           register={register('password')}
           errorMessage={formState?.errors?.['password']?.message}
           required
-          className="my-6"
+          inputType="borderBottom"
+          className="my-4 sm:my-3 md:my-6 2xl:my-4"
           suffixContent={
             passwordShown ? (
               <span onClick={onTogglePassword}>
@@ -58,12 +67,13 @@ const SignupStep3 = ({onSubmit}: StepProps) => {
           label="Confirm password"
           type="password"
           placeholder="Confirm password"
-          className="my-6"
+          className="my-4 sm:my-3 md:my-6 2xl:my-4"
           register={register('confirmPassword')}
           errorMessage={formState?.errors?.['confirmPassword']?.message}
           required
+          inputType="borderBottom"
         />
-        <div className="grid w-full grid-cols-2  gap-3 py-5">
+        <div className="grid w-full grid-cols-4  gap-3 py-5 sm:py-3 md:py-5 2xl:py-3">
           <div
             className={twMerge(
               'flex flex-col  border-t-4 border-t-success py-3',
@@ -80,11 +90,27 @@ const SignupStep3 = ({onSubmit}: StepProps) => {
           >
             <p className="text-sm">・1 number </p>
           </div>
+          <div
+            className={twMerge(
+              'flex flex-col  border-t-4 border-t-success py-3',
+              !isValidPasswordHasUpCase && 'border-opacity-40',
+            )}
+          >
+            <p className="text-sm">・1 uppercase </p>
+          </div>
+          <div
+            className={twMerge(
+              'flex flex-col border-t-4 border-t-success py-3',
+              !isValidPasswordHasLowCase && 'border-opacity-40',
+            )}
+          >
+            <p className="text-sm">・1 lowercase </p>
+          </div>
         </div>
       </div>
-      <div className="-mx-16 divide-x border-t-2 border-b-grayLineBased pl-10 sm:h-48 sm:pl-0 ">
+      <div className="sm:min-h-48 -mx-16 divide-x border-t-2 border-b-grayLineBased pl-10 pb-12 sm:pl-0">
         <Button
-          className="m-auto mt-4  mb-12 flex w-full max-w-xs items-center justify-center  align-middle sm:mb-auto"
+          className="m-auto mt-4 flex w-full max-w-xs items-center justify-center  align-middle sm:mb-auto"
           type="submit"
           size="lg"
           variant="fill"
