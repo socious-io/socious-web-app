32183dfce98f8c998da85432f6943a7f6560cc17
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2022-09-27 10:42:27 +0800

    chore: initial commit for capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.socious.network',
+  appName: 'Socious',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
