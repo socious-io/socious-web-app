d4598894fc8b3e3cf99c2c74e35d7f077f90dce7
Babin Bohora <bohorababin1@gmail.com>
2022-09-28 22:28:06 +0900

    First change for layout

--- layout/PreAuthLayout/index.tsx
+++ layout/PreAuthLayout/index.tsx
@@ -1,7 +1,11 @@
 import {FC, PropsWithChildren} from 'react';
 
 const PreAuthLayout: FC<PropsWithChildren> = ({children}) => {
-  return <div className="sm:pt-24">{children}</div>;
+  return (
+    <div className="sm:flex sm:min-h-screen sm:items-center sm:justify-center">
+      {children}
+    </div>
+  );
 };
 
 export default PreAuthLayout;
