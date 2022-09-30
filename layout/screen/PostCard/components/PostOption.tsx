19ad2ff5fa775f1daf3f7b1fa1bd05a105a2d040
Babin Bohora <bohorababin1@gmail.com>
2022-09-30 13:50:29 +0900

    Refactored PostOption with []

--- layout/screen/PostCard/components/PostOption.tsx
+++ layout/screen/PostCard/components/PostOption.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import {PencilIcon, ShareIcon, TrashIcon} from '@heroicons/react/24/outline';
-import {Button} from '@components/common';
-import {Menu, Transition} from '@headlessui/react';
-import {EllipsisHorizontalIcon} from '@heroicons/react/24/solid';
+/* This components is contains options for post that can be
+ * access only by authorized owner of the post.
+ */
+
+// Packages
+import {Fragment} from 'react';
 import {twMerge} from 'tailwind-merge';
 
+// Icons
+import {PencilIcon, ShareIcon, TrashIcon} from '@heroicons/react/24/outline';
+import {EllipsisHorizontalIcon} from '@heroicons/react/24/solid';
+
+//Components
+import {Menu, Transition} from '@headlessui/react';
+import {Button} from '@components/common';
+
+// Types
+type OptionActionType = 'EDIT' | 'SHARE' | 'DELETE';
 interface PostOptionProps {
-  optionClicked: (data: 'EDIT' | 'SHARE' | 'DELETE') => void;
+  optionClicked: (data: OptionActionType) => void;
 }
+
+type OptionType = {
+  action: OptionActionType;
+  label: string;
+  icon: React.ReactElement;
+};
+
+// Options
+const options: OptionType[] = [
+  {action: 'EDIT', label: 'Edit post', icon: <PencilIcon className="w-4" />},
+  {action: 'SHARE', label: 'Share post', icon: <ShareIcon className="w-4" />},
+  {action: 'DELETE', label: 'Delete post', icon: <TrashIcon className="w-4" />},
+];
 
 const PostOption = ({optionClicked}: PostOptionProps) => {
   return (
     <Menu>
-      <Menu.Button as={React.Fragment}>
+      <Menu.Button as={Fragment}>
         <Button variant="ghost" className="flex border-0 p-2">
           <EllipsisHorizontalIcon className="h-5 w-5" />
         </Button>
       </Menu.Button>
       <Transition
-        as={React.Fragment}
+        as={Fragment}
         enter="transition duration-200 ease-out"
         enterFrom="transform scale-60 opacity-0"
         enterTo="transform scale-100 opacity-100"
@@ -27,53 +51,23 @@ const PostOption = ({optionClicked}: PostOptionProps) => {
         leaveTo="transform scale-60 opacity-0"
       >
         <Menu.Items className="absolute top-12 right-4 z-10 w-52 overflow-hidden rounded-2xl border border-grayLineBased bg-white text-base drop-shadow-lg">
-          <Menu.Item>
-            {({active}) => (
-              <Button
-                className={twMerge(
-                  'flex w-full items-center space-x-3 rounded-none font-normal text-black',
-                  active && 'bg-blue-100',
-                )}
-                variant="link"
-                onClick={() => optionClicked('EDIT')}
-              >
-                <PencilIcon className="w-4" />
-                <span>Edit post</span>
-              </Button>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({active}) => (
-              <Button
-                // className="flex w-full items-center space-x-3 font-normal text-black"
-                className={twMerge(
-                  'flex w-full items-center space-x-3 rounded-none font-normal text-black',
-                  active && 'bg-blue-100',
-                )}
-                variant="link"
-                onClick={() => optionClicked('SHARE')}
-              >
-                <ShareIcon className="w-4" />
-                <span>Share post</span>
-              </Button>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({active}) => (
-              <Button
-                // className="flex w-full items-center space-x-3 font-normal text-black"
-                className={twMerge(
-                  'flex w-full items-center space-x-3 rounded-none font-normal text-black',
-                  active && 'bg-blue-100',
-                )}
-                variant="link"
-                onClick={() => optionClicked('DELETE')}
-              >
-                <TrashIcon className="w-4" />
-                <span>Delete post</span>
-              </Button>
-            )}
-          </Menu.Item>
+          {options.map((option) => (
+            <Menu.Item as={Fragment} key={option.action}>
+              {({active}) => (
+                <Button
+                  className={twMerge(
+                    'flex w-full items-center space-x-3 rounded-none font-normal text-black',
+                    active && 'bg-blue-100',
+                  )}
+                  variant="link"
+                  onClick={() => optionClicked(option.action)}
+                >
+                  {option.icon}
+                  <span>{option.label}</span>
+                </Button>
+              )}
+            </Menu.Item>
+          ))}
         </Menu.Items>
       </Transition>
     </Menu>
