93b9a88740fab467430b7bc27d77b0b68efef032
Dani Manalang <dani@techtank.ca>
2022-10-04 02:05:03 +0800

    FEAT-01: Fix headerbox style to align with figma

--- components/common/Project/component/HeaderBox.tsx
+++ components/common/Project/component/HeaderBox.tsx
@@ -7,6 +7,7 @@ type Props = {
   isExpandable: boolean;
   isRound: boolean;
   expandToggle: () => void;
+  isLastItem: boolean;
 };
 
 function HeaderBox({
@@ -15,26 +16,30 @@ function HeaderBox({
   isRound,
   expandToggle,
   isExpand,
+  isLastItem,
 }: Props) {
   return (
     <div
       className={twMerge(
-        'space-y-6 rounded-tl-2xl rounded-tr-2xl border border-grayLineBased  bg-white p-4',
+        'space-y-6 rounded-tl-2xl rounded-tr-2xl bg-white p-4 shadow-sm',
         !isRound && 'rounded-none rounded-tr-none',
+        isLastItem &&
+          !isExpand &&
+          'rounded-bl-2xl rounded-br-2xl rounded-tl-none rounded-tr-none',
       )}
     >
       <div className="flex flex-row items-center justify-between space-x-2">
-        <p className="font-semibold">{title}</p>
+        <p className="font-worksans font-semibold">{title}</p>
         {isExpandable && (
           <span onClick={expandToggle}>
             {isExpand ? (
               <ChevronUpIcon
-                className="h-5 w-5 text-gray-400"
+                className="h-5 w-5 fill-[#020305] text-gray-400"
                 aria-hidden="true"
               />
             ) : (
               <ChevronDownIcon
-                className="h-5 w-5 text-gray-400"
+                className="h-5 w-5 fill-[#020305] text-gray-400"
                 aria-hidden="true"
               />
             )}
