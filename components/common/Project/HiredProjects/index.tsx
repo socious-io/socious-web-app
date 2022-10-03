9a5adad289a3c78cd0b51d035c2212747f8b0ce1
Dani Manalang <dani@techtank.ca>
2022-10-04 02:03:56 +0800

    FEAT-01: Add on-going and completed tabs and removed other tabs

--- components/common/Project/HiredProjects/index.tsx
+++ components/common/Project/HiredProjects/index.tsx
@@ -24,53 +24,53 @@ var data2 = [
     id: 2,
     projectId: '2',
   },
-  {
-    id: 3,
-    projectId: '3',
-  },
 ];
 
 function HiredProjectsBoxes() {
-  const {state: showPending, handlers: showPendingHandler} = useToggle();
-  const {state: showDecline, handlers: showDeclineHandler} = useToggle();
-  const {state: showAwaiting, handlers: showAwaitingHandler} = useToggle();
+  const {state: showOnGoing, handlers: showOnGoingHandler} = useToggle();
+  const {state: showCompleted, handlers: showCompletedHandler} = useToggle();
 
   return (
-    <div className="w-full rounded-2xl border border-grayLineBased pb-4 ">
-      <HeaderBox
-        title={'pending'}
-        isExpand={showPending}
-        expandToggle={showPendingHandler.toggle}
-        isExpandable={true}
-        isRound={true}
-      />
-      {data.map((item) => (
-        <BodyCard
-          key={item.id}
-          refixAddress={`/app/projects/hired/section/${item.projectId}`}
-        />
-      ))}
-      <HeaderBox
-        title={'Awaiting review 3'}
-        isExpand={showAwaiting}
-        expandToggle={showAwaitingHandler.toggle}
-        isExpandable={true}
-        isRound={true}
-      />
-      {showAwaiting &&
-        data2.map((item) => (
-          <BodyCard
-            key={item.id}
-            refixAddress={`/app/projects/hired/section/${item.projectId}`}
+    <div className="w-full">
+      <div className="mb-4 w-full space-y-6  rounded-2xl border border-grayLineBased bg-white p-6 shadow-sm">
+        <span className="font-worksans text-xl font-semibold">
+          Hired Projects
+        </span>
+      </div>
+      <div className="w-full">
+        <div className="w-full rounded-2xl border border-grayLineBased">
+          <HeaderBox
+            title={`On-Going (${data.length})`}
+            isExpand={showOnGoing}
+            expandToggle={showOnGoingHandler.toggle}
+            isExpandable={true}
+            isRound={true}
+            isLastItem={false}
           />
-        ))}
-      <HeaderBox
-        title={'Declined'}
-        isExpand={showDecline}
-        expandToggle={showDeclineHandler.toggle}
-        isExpandable={true}
-        isRound={true}
-      />
+          {showOnGoing &&
+            data.map((item) => (
+              <BodyCard
+                key={item.id}
+                refixAddress={`/app/projects/hired/section/${item.projectId}`}
+              />
+            ))}
+          <HeaderBox
+            title={`Completed (${data2.length})`}
+            isExpand={showCompleted}
+            expandToggle={showCompletedHandler.toggle}
+            isExpandable={true}
+            isRound={true}
+            isLastItem
+          />
+          {showCompleted &&
+            data2.map((item) => (
+              <BodyCard
+                key={item.id}
+                refixAddress={`/app/projects/hired/section/${item.projectId}`}
+              />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
