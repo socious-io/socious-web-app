bb7fbfe74542ba0abcfa5d8dab91f1046cdaa899
Dani Manalang <dani@techtank.ca>
2022-10-04 02:04:21 +0800

    FEAT-01: Fix border and box shadow styles for each body card

--- components/common/Project/component/BodyCard.tsx
+++ components/common/Project/component/BodyCard.tsx
@@ -8,16 +8,20 @@ type Props = {
 function BodyCard({refixAddress}: Props) {
   return (
     <Link href={refixAddress ? refixAddress : 'javascript:;'}>
-      <div className="m-4 space-y-6 rounded-2xl border border-grayLineBased  bg-white p-4">
+      <div className="m-4 rounded-2xl border border-grayLineBased  bg-white p-4">
         <div className="">
-          <p className="font-semibold">Project Title</p>
+          <p className="font-worksans font-semibold text-primary">
+            Project Title
+          </p>
         </div>
-        <div className="flex flex-row items-center  space-x-2">
+        <div className="mt-2 flex flex-row items-center space-x-2">
           <Avatar size="l" />
-          <p className="text-black">Organization</p>
+          <p className="font-worksans font-semibold text-black">Organization</p>
         </div>
         <div>
-          <p className="my-4 text-sm">Mar 1 -Mar 10</p>
+          <p className="font-worksans my-2 text-sm text-graySubtitle">
+            Mar 1 - Mar 10
+          </p>
         </div>
       </div>
     </Link>
