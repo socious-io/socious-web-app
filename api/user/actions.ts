54608af812b39107e8bfa5206cf0bf2e0c711bad
Babin Bohora <bohorababin1@gmail.com>
2022-10-05 18:36:50 +0900

    using existing method

--- api/user/actions.ts
+++
@@ -1,6 +0,0 @@
-import {UpdateProfileBodyType} from '@models/profile';
-import {post} from 'utils/request';
-
-export function updateProfile(profileBody: UpdateProfileBodyType) {
-  return post('/user/update/profile', profileBody);
-}
