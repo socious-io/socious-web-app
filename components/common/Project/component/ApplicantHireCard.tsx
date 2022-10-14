8acfcfcf90ce7c9178f715bcab80335b8ed69d09
Babin Bohora <bohorababin1@gmail.com>
2022-10-14 11:39:48 +0900

    Refactored with real data. Redirect to msg.

--- components/common/Project/component/ApplicantHireCard.tsx
+++ components/common/Project/component/ApplicantHireCard.tsx
@@ -1,26 +1,48 @@
 import {Avatar} from '@components/common';
 import {Button} from '@components/common';
 import Image from 'next/image';
+import Link from 'next/link';
+import Router from 'next/router';
 
 const endhireSrc = require('../../../../asset/icons/endhire.svg');
 const messageSrc = require('../../../../asset/icons/message.svg');
+const rejectSrc = require('../../../../asset/icons/reject.svg');
 
-function ApplicantHiredCard() {
+// Types
+type ApplicantHireCardProps = {
+  name: string;
+  username?: string;
+  bio: string;
+  status: string;
+  userId: string;
+  applicantId: string;
+  avatar?: string;
+};
+function ApplicantHiredCard({
+  name,
+  username,
+  bio,
+  status,
+  userId,
+  applicantId,
+  avatar,
+}: ApplicantHireCardProps) {
   return (
     <div className="my-4 space-y-6 rounded-2xl border border-grayLineBased bg-white p-4">
       <div className="flex flex-row items-center justify-between ">
         <div className="flex flex-row space-x-2">
-          <Avatar size="l" />
+          <Avatar size="xl" src={avatar} type={'users'} />
           <div className="flex flex-col">
-            <p className="text-black">Organization</p>
-            <p className="text-graySubtitle">Location</p>
+            <p className="text-2xl font-semibold text-black">{name}</p>
+            <Link href={`/app/user/${username}`}>
+              <p className="cursor-pointer text-primary">View profile</p>
+            </Link>
           </div>
         </div>
       </div>
       <div className="grid grid-cols-2 items-center rounded-2xl bg-offWhite p-4">
         <p className="text-black">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Equat
-          faucibus sed facilisi sit id blandiacilisi sit id blandit... See more
+          {bio?.length > 200 ? `${bio?.slice(0, 50)}...` + 'See more' : bio}
         </p>
       </div>
 
@@ -40,33 +62,36 @@ function ApplicantHiredCard() {
               layout="fill" // required
             />
           </div>
-          End hire
+          {status === 'PENDING' ? 'Hire' : 'End hire'}
         </Button>
+        {status === 'PENDING' && (
+          <Button
+            className="m-auto mt-4  flex w-full max-w-xs items-center justify-center align-middle "
+            type="submit"
+            size="lg"
+            variant="outline"
+            value="Submit"
+          >
+            <div className="relative mx-2 h-5 w-5 ">
+              <a>
+                <Image
+                  src={rejectSrc}
+                  className="fill-warning"
+                  alt="dislike"
+                  layout="fill" // required
+                />
+              </a>
+            </div>
+            reject
+          </Button>
+        )}
         <Button
           className="m-auto mt-4  flex w-full max-w-xs items-center justify-center align-middle "
           type="submit"
           size="lg"
           variant="outline"
           value="Submit"
-        >
-          <div className="relative mx-2 h-5 w-5 ">
-            <a>
-              <Image
-                src={endhireSrc}
-                className="fill-warning"
-                alt="dislike"
-                layout="fill" // required
-              />
-            </a>
-          </div>
-          reject
-        </Button>
-        <Button
-          className="m-auto mt-4  flex w-full max-w-xs items-center justify-center align-middle "
-          type="submit"
-          size="lg"
-          variant="outline"
-          value="Submit"
+          onClick={() => Router.push(`/app/chat/create/${userId}`)}
         >
           <div className="relative mx-2 h-5 w-5 ">
             <a>
