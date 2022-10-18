d11bae8c0b7b969b3882dedb7d9eb8422dc4a165
Babin Bohora <bohorababin1@gmail.com>
2022-10-18 21:58:39 +0900

    Removed wrapper for ProjectDetail in Preview

--- components/common/Search/Preview/Preview.tsx
+++ components/common/Search/Preview/Preview.tsx
@@ -1,9 +1,10 @@
 import {FC} from 'react';
 import {twMerge} from 'tailwind-merge';
 import {OrganizationPreview} from './OrganizationPreview';
-import {ProjectPreview} from './ProjectPreview';
+// import {ProjectPreview} from './ProjectPreview';
+import ProjectDetail from '@components/common/Project/MainContent/DetailContent';
 import {UserPreview} from './UserPreview';
-
+import {useSkills} from '../Providers/SkillsProvider';
 interface SearchResultPreviewProps {
   type: string;
   id: string;
@@ -15,12 +16,20 @@ export const SearchResultPreview: FC<SearchResultPreviewProps> = ({
   id,
   className,
 }) => {
+  const {skills} = useSkills();
+
   const renderPreview = () => {
     switch (type) {
       case 'organizations':
         return <OrganizationPreview id={id} />;
       case 'projects':
-        return <ProjectPreview id={id} />;
+        return (
+          <ProjectDetail
+            skills={skills}
+            projectId={id}
+            className="rounded-none border-0"
+          />
+        );
       case 'users':
         return <UserPreview id={id} />;
       default:
