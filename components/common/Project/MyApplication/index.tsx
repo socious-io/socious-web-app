e75cd65d7935c807e8db66283eb95de6e18927f6
misa <q.misa@yahoo.com>
2022-10-19 15:39:25 +0200

    #141_Project_user : apply changes based on SWR

--- components/common/Project/MyApplication/index.tsx
+++ components/common/Project/MyApplication/index.tsx
@@ -1,68 +1,57 @@
-import {useEffect, useState} from 'react';
+import useSWR from 'swr';
 import BodyCard from '../component/BodyCard';
 import HeaderBox from '../component/HeaderBox';
 import {useToggle} from '@hooks';
-import {getUserProjects} from '@api/user/actions';
 import {IUserProjects} from '@models/project';
+import {get} from 'utils/request';
+
+// todo : type for groupByStatus
+let groupByStatus: any;
 
 function MyApplicationBoxes() {
   const {state: showPending, handlers: showPendingHandler} = useToggle();
   const {state: showDecline, handlers: showDeclineHandler} = useToggle();
   const {state: showAwaiting, handlers: showAwaitingHandler} = useToggle();
 
-  const [userProjects, setUserProjects] = useState({
-    PENDING: [],
-    AWAITING: [],
-    DECLINE: [],
-  });
+  const {data, error} = useSWR<any>('/user/applicants', get);
 
-  const fetchUserProjects = async () => {
-    try {
-      const response: any = await getUserProjects();
-      const {items} = response;
-      const groupByStatus = items.reduce((group: any, item: IUserProjects) => {
-        const {status} = item;
-        group[status] = group[status] ?? [];
-        group[status].push(item);
-        return group;
-      }, {});
-      setUserProjects(groupByStatus);
-    } catch (error) {
-      console.error(error);
-    }
+  const fetchUserProjects = () => {
+    const {items} = data;
+    groupByStatus = items.reduce((group: any, item: IUserProjects) => {
+      const {status} = item;
+      group[status] = group[status] ?? [];
+      group[status].push(item);
+      return group;
+    }, {});
   };
 
-  useEffect(() => {
+  if (data) {
     fetchUserProjects();
-  }, []);
+  }
+  if (!data && !error) return <p>loading</p>;
 
   return (
     <div className="w-full rounded-2xl border border-grayLineBased pb-4 ">
       <HeaderBox
-        title={`pending (${userProjects?.PENDING?.length ?? 0})`}
+        title={`pending (${groupByStatus?.PENDING?.length ?? 0})`}
         isExpand={showPending}
         expandToggle={showPendingHandler.toggle}
         isExpandable={true}
         isRound={true}
       />
       {showPending &&
-        userProjects?.PENDING.map((item: IUserProjects) => (
-          <BodyCard
-            key={item.id}
-            name={item.cover_letter}
-            item={item}
-            // refixAddress={`/app/projects/applications/section/${item.projectId}`}
-          />
+        groupByStatus?.PENDING?.map((item: IUserProjects) => (
+          <BodyCard key={item.id} name={item.cover_letter} item={item} />
         ))}
       <HeaderBox
-        title={`Awaiting review (${userProjects?.AWAITING?.length ?? 0})`}
+        title={`Awaiting review (${groupByStatus?.AWAITING?.length ?? 0})`}
         isExpand={showAwaiting}
         expandToggle={showAwaitingHandler.toggle}
         isExpandable={true}
         isRound={true}
       />
       {showAwaiting &&
-        userProjects?.AWAITING?.map((item: IUserProjects) => (
+        groupByStatus?.AWAITING?.map((item: IUserProjects) => (
           <BodyCard
             key={item.id}
             name={item.cover_letter}
@@ -71,14 +60,14 @@ function MyApplicationBoxes() {
           />
         ))}
       <HeaderBox
-        title={`Declined (${userProjects?.DECLINE?.length ?? 0})`}
+        title={`Declined (${groupByStatus?.DECLINE?.length ?? 0})`}
         isExpand={showDecline}
         expandToggle={showDeclineHandler.toggle}
         isExpandable={true}
         isRound={true}
       />
       {showDecline &&
-        userProjects?.DECLINE?.map((item: IUserProjects) => (
+        groupByStatus?.DECLINE?.map((item: IUserProjects) => (
           <BodyCard key={item.id} name={item.cover_letter} item={item} />
         ))}
     </div>
