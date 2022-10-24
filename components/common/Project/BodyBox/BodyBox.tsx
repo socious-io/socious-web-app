ec56222d6f8e11307092cc158d41ecc0ae11b9ad
Babin Bohora <bohorababin1@gmail.com>
2022-10-24 12:45:07 +0900

    Made BodyBox reusablec

--- components/common/Project/BodyBox/BodyBox.tsx
+++ components/common/Project/BodyBox/BodyBox.tsx
@@ -1,22 +1,27 @@
 import {useState} from 'react';
 import Markdown from 'markdown-to-jsx';
+import {twMerge} from 'tailwind-merge';
 
 interface Props {
-  title: string;
+  title?: string;
   description: string;
+  className?: string;
+  minCount?: number;
 }
 
-function BodyBox({title, description}: Props) {
+function BodyBox({title, description, className, minCount = 500}: Props) {
   const [showMore, setShowMore] = useState<boolean>(false);
 
   return (
-    <div className="flex w-full flex-col p-4">
-      <label className="font-semibold text-black">{title}</label>
-      {description?.length > 500 ? (
+    <div
+      className={twMerge('flex w-full flex-col p-4', className && className)}
+    >
+      {title && <label className="font-semibold text-black">{title}</label>}
+      {description?.length > minCount ? (
         <p>
           {!showMore ? (
             <Markdown options={{wrapper: 'article'}}>
-              {description?.slice?.(0, 500)}
+              {description?.slice?.(0, minCount)}
             </Markdown>
           ) : (
             <Markdown options={{wrapper: 'article'}}>{description}</Markdown>
