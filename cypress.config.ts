53bfd35ef31688d21ac1132cf108f76af6e7086c
Lalo Martins <lalo.martins@gmail.com>
2022-10-28 18:49:01 +0300

    clean up dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
-const {defineConfig} = require('cypress');
+import {defineConfig} from 'cypress';
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
