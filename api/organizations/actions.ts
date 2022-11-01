9853d272bf24c1f7b5ff4b674fd577bdcfdf14a1
Babin Bohora <bohorababin1@gmail.com>
2022-11-01 20:16:13 +0900

    ADDED editModal and endpoint

--- api/organizations/actions.ts
+++ api/organizations/actions.ts
@@ -1,5 +1,10 @@
-import {get} from 'utils/request';
+import {IUpdateOrgBody} from '@models/profile';
+import {get, post} from 'utils/request';
 
 export function getOrganization(identityId: string) {
   return get(`/orgs/${identityId}`);
+}
+
+export function updateOrganization(orgId: string, orgBody: IUpdateOrgBody) {
+  return post(`/orgs/update/${orgId}`, orgBody);
 }
--- pages/app/organization/[id]/index.tsx
+++ pages/app/organization/[id]/index.tsx
@@ -2,28 +2,49 @@
  * user profile page with dynamic route
  */
 
-import React from 'react';
-import type {NextPage} from 'next';
+import React, {useCallback} from 'react';
+import type {GetStaticPaths, GetStaticProps, NextPage} from 'next';
 import {useRouter} from 'next/router';
 
 //libraries
 import useSWR from 'swr';
+import {Libraries, useGoogleMapsScript} from 'use-google-maps-script';
 
 //components
 import {GeneralLayout} from 'layout';
 import MainContent from '@components/common/UserProfile/MainContent';
+import SplashScreen from 'layout/Splash';
+import EditProfileModal from '@components/common/UserProfile/Edit/EditProfileModal/EditProfileModal';
 
 //utils
 import {get} from 'utils/request';
+import {useToggle, useUser} from '@hooks';
+import getGlobalData from 'services/cacheSkills';
 
 // Type
 import {IOrganizationType} from 'models/organization';
-import SplashScreen from 'layout/Splash';
+import {Skill} from '@components/common/Search/Providers/SkillsProvider';
+type OrganizationProfileProps = {
+  skills: Skill[];
+};
+// Libraries
+const libraries: Libraries = ['places'];
 
-const OrganizationProfile: NextPage = () => {
+const OrganizationProfile: NextPage<OrganizationProfileProps> = ({skills}) => {
   // get id from route
   const router = useRouter();
   const {id} = router.query;
+  const {state: editState, handlers: editHandlers} = useToggle();
+
+  const {user} = useUser();
+
+  // Loading The Map
+  const {isLoaded} = useGoogleMapsScript({
+    googleMapsApiKey: process.env['NEXT_PUBLIC_GOOGLE_API_KEY'] ?? '',
+    libraries,
+  });
+
+  const openEditModal = useCallback(() => editHandlers.on(), [editHandlers]);
 
   //get user profile data by user id
   const {data, mutate, error} = useSWR<IOrganizationType>(
@@ -44,6 +65,8 @@ const OrganizationProfile: NextPage = () => {
 
   if (!data) return <SplashScreen />;
 
+  console.log('User :---: ', user);
+
   return (
     <GeneralLayout>
       <div className="flex w-full flex-col justify-center md:flex-row  md:px-8  lg:px-0 ">
@@ -51,10 +74,32 @@ const OrganizationProfile: NextPage = () => {
           data={data}
           status="organizations"
           profile_mutate={mutate}
+          editProfile={openEditModal}
         />
       </div>
+      {/* EDIT PROFILE */}
+      {isLoaded && user && user.id === data?.id && (
+        <EditProfileModal
+          openState={editState}
+          user={user}
+          skillsData={skills}
+          closeModal={editHandlers.off}
+        />
+      )}
     </GeneralLayout>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const skills = await getGlobalData();
+  return {props: {skills}, revalidate: 60};
 };
 
 export default OrganizationProfile;
