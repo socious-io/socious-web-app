5d685350afd11e5b6537aeff6b2ffa2b15df925f
Lalo Martins <lalo.martins@gmail.com>
2022-11-01 16:00:40 +0200

    add a sample Gherkin file

---
+++ cypress/support/step_definitions/setups/viewing-page.js
@@ -0,0 +1,6 @@
+import {Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
+
+Given("{word} is viewing {word}'s profile", function (actor, other) {
+  // log in as actor, navigate to other's profile
+  return 'pending';
+});
