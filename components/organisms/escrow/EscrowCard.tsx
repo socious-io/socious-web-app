3d26593a33649ca8ad44f7f8aa2f3a66877aeb05
Babin Bohora <bohorababin1@gmail.com>
2022-11-07 11:52:56 +0900

    Build fix

--- components/organisms/escrow/EscrowCard.tsx
+++ components/organisms/escrow/EscrowCard.tsx
@@ -8,7 +8,7 @@ import {
 import {TOffer} from '@models/offer';
 
 type EscrowCardType = {
-  offer: TOffer;
+  offer?: TOffer;
 };
 
 const EscrowCard = ({offer}: EscrowCardType) => {
@@ -25,7 +25,7 @@ const EscrowCard = ({offer}: EscrowCardType) => {
       </div>
       <div className="flex items-center justify-between">
         <p className="my-4 text-sm">{'Mar 1 - Present'}</p>
-        {offer.status === 'APPROVED' && (
+        {offer?.status === 'APPROVED' && (
           <p className="inline-flex gap-2 rounded-2xl bg-error py-1 px-2 text-white">
             <ExclamationCircleIcon className="w-5" />
             Payment required
