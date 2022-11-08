43b88614ae3224fa6e31b2934bea46742a548be7
Babin Bohora <bohorababin1@gmail.com>
2022-11-08 14:46:11 +0900

    Type for offer

---
+++ models/offer.ts
@@ -0,0 +1,18 @@
+export interface IOffer {
+  id: string;
+  project_id: string;
+  recipient_id: string;
+  offerer_id: string;
+  applicant_id: string | null;
+  assignment_total: string | null;
+  offer_rate: string | null;
+  offer_message: string;
+  status: string;
+  payment_type: string | null;
+  due_date: string | null;
+  payment_scheme: string | null;
+  weekly_limit: string | null;
+  total_hours: number;
+  created_at: string;
+  updated_at: string;
+}
