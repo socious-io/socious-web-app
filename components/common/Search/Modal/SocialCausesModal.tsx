843d11208cf61c852bd2d3846d460f67d85303a6
Babin Bohora <bohorababin1@gmail.com>
2022-11-14 11:24:26 +0900

    Fixed social causes

--- components/common/Search/Modal/SocialCausesModal.tsx
+++ components/common/Search/Modal/SocialCausesModal.tsx
@@ -4,7 +4,7 @@ import Modal from '@components/common/Modal/Modal';
 import {FromLayout} from '@components/common/Project/created/NewProject/Layout';
 import {XMarkIcon} from '@heroicons/react/24/solid';
 import {useRouter} from 'next/router';
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {FieldValues, FormProvider, useForm} from 'react-hook-form';
 
 interface SocialCausesModalProps {
@@ -18,6 +18,13 @@ export const SocialCausesModal: FC<SocialCausesModalProps> = ({
 }) => {
   const route = useRouter();
   const methods = useForm();
+  const {social_causes} = route.query;
+
+  useEffect(() => {
+    if (social_causes)
+      methods.setValue('social_causes', (social_causes as string)?.split(','));
+    else methods.setValue('social_causes', null);
+  }, [methods, route.query, social_causes]);
 
   const onsubmit = (data: FieldValues) => {
     route.query.social_causes = data.social_causes.join(',');
