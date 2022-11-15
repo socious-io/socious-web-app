69cf0954409e5abbe0669ea9db29941f8ae5c8d7
Babin Bohora <bohorababin1@gmail.com>
2022-11-15 13:54:24 +0900

    Refactored the code fields with component

--- components/molecules/RegisterModals/SignupStep2Form.tsx
+++ components/molecules/RegisterModals/SignupStep2Form.tsx
@@ -1,8 +1,10 @@
 import {Button, TextInput} from '@components/common';
+import CodeFields from '@components/common/CodeFields/CodeFields';
 import {ExclamationCircleIcon} from '@heroicons/react/24/solid';
 import {StepProps} from '@models/stepProps';
 import Link from 'next/link';
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {useFormContext} from 'react-hook-form';
 import {useRegisterContext} from './RegisterContext';
 
 type SignupStep2FormProps = StepProps & {
@@ -11,67 +13,56 @@ type SignupStep2FormProps = StepProps & {
 
 const SignupStep2Form = ({onSubmit, resendCode}: SignupStep2FormProps) => {
   const {setRegisterContext} = useRegisterContext();
-  const [code, setCode] = useState<any>([null, null, null, null]);
-  const codeInputRef = useRef<any>(null);
+  const {
+    formState: {errors},
+    watch,
+    clearErrors,
+  } = useFormContext();
+  const email = watch('email');
+
+  const [code, setCode] = useState<string | null>(null);
+
   const [blockVerify, setBlockVerify] = useState<boolean>(true);
 
-  useEffect(
-    () => setBlockVerify(() => code.includes(null) || code.includes('')),
-    [code],
+  const handleBlockVerify = useCallback(
+    (state: boolean) => setBlockVerify(state),
+    [],
+  );
+
+  const handleCodeChange = useCallback(
+    (newCode: string) => {
+      setCode(newCode);
+      if (errors['code']?.message) clearErrors('code');
+    },
+    [clearErrors, errors],
   );
 
   const handleSubmitCheckCode = (e: any) => {
     e.preventDefault();
-    onSubmit(code?.join(''));
-  };
-
-  const handleCodeInputChange = (e: any, codeIndex: number) => {
-    setCode(
-      code?.map((codeItem: any, indexCodeItem: number) =>
-        indexCodeItem === codeIndex
-          ? e.target.value !== ''
-            ? Math.max(0, parseInt(e.target.value)).toString().slice(0, 1)
-            : null
-          : code[indexCodeItem],
-      ),
-    );
-
-    codeIndex !== 3 &&
-      e.target.value !== '' &&
-      codeInputRef.current?.children[codeIndex + 1].children[0].select();
+    onSubmit(code);
   };
 
   return (
     <div className="py-16 px-6 sm:px-[72px]">
       <h1 className="m-0 text-2xl font-semibold">Please check your email</h1>
-      <p className="mb-10 text-graySubtitle ">
-        We’ve sent a code to info@socious.io
-      </p>
+      <p className="mb-10 text-graySubtitle ">We’ve sent a code to {email}.</p>
       <form
         onSubmit={handleSubmitCheckCode}
         className="flex grow flex-col justify-between sm:grow-0"
       >
         <div className="flex grow flex-col space-y-4">
-          <div
-            className="codes mx-auto flex  flex-row justify-center space-x-3 pt-10 pb-5"
-            ref={codeInputRef}
-          >
-            {[0, 1, 2, 3].map((codeIndex) => (
-              <TextInput
-                key={`opt-code-${codeIndex}`}
-                value={code[codeIndex]}
-                onChange={(e) => handleCodeInputChange(e, codeIndex)}
-                type="number"
-                maxLength={1}
-                className="h-16 w-16 border-2 border-grayLineBased text-center "
-              />
-            ))}
-          </div>
-          {/* {error && (
+          <CodeFields
+            sendCode={handleCodeChange}
+            setBlock={handleBlockVerify}
+          />
+          {errors && errors['code']?.message && (
             <p className="text-error">
-              <ExclamationCircleIcon className="inline h-5 w-5" /> {error}
+              <>
+                <ExclamationCircleIcon className="inline h-5 w-5" />{' '}
+                {errors['code']?.message}
+              </>
             </p>
-          )} */}
+          )}
           <p className="text-graySubtitle">
             Didn’t received a code?
             <Button variant="link" onClick={() => resendCode()}>
@@ -86,7 +77,7 @@ const SignupStep2Form = ({onSubmit, resendCode}: SignupStep2FormProps) => {
             size="lg"
             variant="fill"
             value="Submit"
-            disabled={blockVerify}
+            disabled={blockVerify || !!errors['code']?.message}
           >
             Verify email
           </Button>
