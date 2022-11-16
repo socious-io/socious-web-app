46d79e5827e1a00e898a4bea2ceebc10578b70e3
Babin Bohora <bohorababin1@gmail.com>
2022-11-16 14:47:17 +0900

    Using infiniteSWR for loading Notifications

--- components/common/Notification/NotificationContainer.tsx
+++ components/common/Notification/NotificationContainer.tsx
@@ -1,28 +1,54 @@
+import {useState} from 'react';
+import {GridLoader} from 'react-spinners';
+
+// Components
 import Button from '@components/common/Button/Button';
 import Notification from './Notification';
-import {useState} from 'react';
+
+// Hooks
+import useInfiniteSWR from 'hooks/useInfiniteSWR/useInfiniteSWR';
+
+// Types
+import {NotificationItem} from '@models/notification';
 
 const NotificationContainer = () => {
   const [page, setPage] = useState<number>(1);
-  const notifications = [];
-  const [fullList, setFullList] = useState<boolean>(false);
 
-  for (let i = 1; i <= page; i++) {
-    notifications.push(
-      <Notification page={i} key={i} onFull={() => setFullList(true)} />,
+  const {flattenData, seeMore, isLoading, loadMore} =
+    useInfiniteSWR<NotificationItem>('/notifications');
+
+  if (isLoading)
+    return (
+      <div className="flex w-full flex-col items-center justify-center">
+        <GridLoader color="#36d7b7" />
+      </div>
     );
-  }
+
+  const totalCount = flattenData.length;
 
   return (
     <div className="w-full px-4 lg:w-4/6 lg:space-y-2 lg:px-0">
-      <div>{notifications}</div>
-      {!fullList && (
+      <div>
+        {flattenData.map((notification, index) => (
+          <Notification
+            key={notification.id}
+            notification={notification}
+            position={
+              index === 0
+                ? 'FIRST'
+                : index + 1 === totalCount
+                ? 'LAST'
+                : undefined
+            }
+          />
+        ))}
+      </div>
+      {seeMore && (
         <div className="flex justify-center">
           <Button
             variant="link"
             className="font-semibold text-primary"
-            onClick={() => setPage(page + 1)}
-            disabled={fullList}
+            onClick={loadMore}
           >
             See more
           </Button>
