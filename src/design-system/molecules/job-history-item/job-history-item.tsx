af3c80988a0d19ccbf7dbd2a97193ff8717a6ed5
Sajad Abbasi <s.abbasi@me.com>
2022-12-15 16:09:13 +0330

    feature: add job-history-item-donation comp

---
+++ src/design-system/molecules/job-history-item/job-history-item.tsx
@@ -0,0 +1,18 @@
+import css from './job-history-item.module.scss';
+// import {JobHistoryItem} from './job-history-item/job-history-item';
+import {JobHistoryItem as JobHistoryItemNormal} from './job-history-item/job-history-item';
+import {JobHistoryItemProps} from './job-history-item.types';
+
+export const JobHistoryItem = (props: JobHistoryItemProps): JSX.Element => {
+  const data = props as Required<JobHistoryItemProps>;
+
+  const isDonation = !!data.amount;
+
+  if (isDonation) {
+    return <JobHistoryItemNormal data={data} />;
+    // return <JobHistoryItemDonation data={data} />;
+  }
+  return <JobHistoryItemNormal data={data} />;
+
+  // return <JobHistoryItemDonation data={data} />;
+};
